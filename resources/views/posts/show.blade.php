35ffcf7670da0affe5e066290c16fabcdd451b4d
mac <drplauska@gmail.com>
2019-11-11 19:11:16 +0200

    Posts can be viewed

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-8">
+			<img src="/storage/{{$post->image}}" alt="{{$post->caption}}" class="w-100">
+		</div>
+	</div>
+</div>
+@endsection
