12a29cf6cae508d8a90e7e18d557df474605c83e
mac <drplauska@gmail.com>
2019-11-14 17:31:20 +0200

    added feed in index page

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	@foreach($posts as $post)
+	<div class="row">
+		<div class="col-8">
+			<img src="{{$post->user->profile->profileImage()}}" alt="{{$post->caption}}" class="w-100">
+		</div>
+		<div class="col-4">
+			<a href="/profile/{{$post->user->id}}">
+				<div class="d-flex align-items-center">
+					<div>
+						<img src="{{$post->user->profile->profileImage()}}" class="rounded-circle w-100" style="max-width: 40px">
+					</div>
+					<div class="pl-3">
+						<div class="font-weight-bold">
+							<span class="text-dark">{{ $post->user->username }}</span>
+							<span class="pl-3"><a href="#">Follow</a></span>
+						</div>
+					</div>
+				</div>
+			</a>
+			<hr />
+			<div>
+				<span class="font-weight-bold">
+					<a href="/profile/{{$post->user->id}}">
+						<span class="text-dark">{{ $post->user->username }}</span>
+					</a>
+				</span> {{ $post->caption }}
+			</div>
+		</div>
+	</div>
+	@endforeach
+</div>
+@endsection
