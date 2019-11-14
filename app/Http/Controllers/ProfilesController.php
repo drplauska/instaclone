95372b9e8c42d1db807592ebcb264cb45d7e02bf
mac <drplauska@gmail.com>
2019-11-14 15:40:23 +0200

    follow functionality

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,8 +9,8 @@ use Intervention\Image\Facades\Image;
 class ProfilesController extends Controller
 {
     public function index(User $user){
-
-		return view('profiles.index', compact('user'));
+		$follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
+		return view('profiles.index', compact('user', 'follows'));
 	}
 
 	public function edit(User $user){
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,7 @@
 				<div class="d-flex pb-3 align-items-center">
 					<div class="h4">{{ $user->username }}</div>
 
-					<follow-button></follow-button>
+					<follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
 				</div>
 
 				@can('update', $user->profile)
@@ -22,9 +22,9 @@
 				<a href="/profile/{{$user->id}}/edit">Edit profile</a>
 			@endcan
 			<div class="d-flex">
-				<div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
-				<div class="pr-5"><strong>23k</strong> followers</div>
-				<div class="pr-5"><strong>153</strong> following</div>
+				<div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+				<div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> followers</div>
+				<div class="pr-5"><strong>{{ $user->following->count() }}</strong> following</div>
 			</div>
 			<div class="pt-3 font-weight-bold">{{ $user->profile->title }}</div>
 			<div>
